895783b36f55c9ac8f10e102a814391d00c8e5a2
YanaBagrova <yanabag@mail.ru>
2022-02-22 21:58:06 +0300

    release 10 localStorage

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -11,11 +11,10 @@ function Input({ rate, kindPrice, chosenCurrency }) {
 
   const dispatch = useDispatch()
 
-
   function addRequest() {
     if (volume) {
-    const timestamp = new Date()
-    dispatch({type: 'ADD_REQUEST', payload: {kindPrice, rate, volume, timestamp, chosenCurrency}})
+      const timestamp = new Date()
+      dispatch({ type: 'ADD_REQUEST', payload: { kindPrice, rate, volume, timestamp, chosenCurrency } })
     }
   }
 
@@ -25,18 +24,40 @@ function Input({ rate, kindPrice, chosenCurrency }) {
     localStorage.setItem('orders', JSON.stringify(requests))
   }
 
+  // если нужно, чтобы информация о заявках сохранялась после перезапуска браузера, тогда вариант ниже в комментариях:
+  // function addRequest() {
+  //   if (volume) {
+  //     const timestamp = new Date()
+  //     if (JSON.parse(localStorage.getItem('orders'))) {
+  //       const orders = JSON.parse(localStorage.getItem('orders'))
+  //       orders.push({ kindPrice, rate, volume, timestamp, chosenCurrency })
+  //       localStorage.setItem('orders', JSON.stringify(orders))
+  //     } else {
+  //       const requests1 = []
+  //       localStorage.setItem('orders', JSON.stringify(requests1))
+  //       const orders = JSON.parse(localStorage.getItem('orders'))
+  //       orders.push({ kindPrice, rate, volume, timestamp, chosenCurrency })
+  //       localStorage.setItem('orders', JSON.stringify(orders))
+  //     }
+  //   }
+  // }
+  // function watchRequests(event) {
+  //   event.target = '_blank'
+  //   window.open('/requests')
+  // }
+
   return (
     <>
-      <p style={{display: 'inline', verticalAlign: "bottom", fontSize: "30px"}}>Volume</p>
+      <p style={{ display: 'inline', verticalAlign: "bottom", fontSize: "30px" }}>Volume</p>
       <input className="class-input"
         onChange={({ target }) => setVolume(target.value)}
         value={volume}
         placeholder='Your text!' />
-        <button className="ok-button" onClick={addRequest}>OK</button>
-        <Link to={`/requests`}>
+      <button className="ok-button" onClick={addRequest}>OK</button>
+      <Link to={`/requests`}>
         <button className="watch-button" onClick={watchRequests}>Watch requests</button>
         {/* <button onClick={localStorage.clear()}>clear</button> */}
-        </Link>
+      </Link>
     </>
   );
 }
